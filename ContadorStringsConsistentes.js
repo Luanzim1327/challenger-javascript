d1d99285061bd3c8b736ed756ccf8b19e97dbf66
Luanzim1327 <luanmas27@gmail.com>
2023-07-21 21:36:13 -0300

    day-1

--- ContadorStringsConsistentes.js
+++ ContadorStringsConsistentes.js
@@ -13,3 +13,9 @@ function ContadorStringsConsistentes (allowed , words) {
 }
 
 console.log(countConsistentStrings(allowed , words));
+
+// pontos que eu acho importante  
+/*
+    1 - Dar um split no array words , para transformar cada item do array em um array 
+    2 - Uso o metódo every() para pegar cada caractere do word.split() e verificar se algum caractere está incluso no array allowed 
+*/
